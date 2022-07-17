70d8f740cb8e46028a8b9e4620ed8554bc350c50
BogdanKorolev <bkorolevjob@gmail.com>
2022-07-17 12:06:36 +0300

    Task21 and Task23code written.

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 23
+Напишите программу,
+которая принимает на вход число (N)
+и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.Write("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+string result = "";
+int interimResult = 0;
+
+while (i <= N)
+{
+    interimResult = i * i * i;
+    result = $"{result}{interimResult}, ";
+    i++;
+}
+Console.WriteLine(result.Remove(result.Length-2, 2));
