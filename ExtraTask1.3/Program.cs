fd39b2446094423ceac61094029a74b2af1b0ea5
BogdanKorolev <bkorolevjob@gmail.com>
2022-07-22 14:46:30 +0300

    ExtraTast1.3 code written

---
+++ ExtraTask1.3/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Задача 3.
+На ввод подаётся рост трёх спортсменов.
+Расположить их от большего к меньшему.
+*/
+
+double a = 2;
+double b = 1.7;
+double c = 1.9;
+Console.WriteLine($"Рост спортсмена №1: {a}");
+Console.WriteLine($"Рост спортсмена №2: {b}");
+Console.WriteLine($"Рост спортсмена №3: {c}");
+
+if (a > b)
+{
+    if (b > c)
+    {
+        Console.WriteLine($"Построение по росту: 1; 2; 3.");
+    }
+    else if (a > c)
+    {
+       Console.WriteLine($"Построение по росту: №1; №3; №2.");
+    }
+    else
+    {
+            Console.WriteLine($"Построение по росту: №3; №1; №2.");
+        }
+}
+else
+{
+    if (a > c)
+    {
+        Console.WriteLine($"Построение по росту: №2; №1; №3.");
+    }
+    else
+    {
+        if (b > c)
+        {
+            Console.WriteLine($"Построение по росту: №2; №3; №1.");
+        }
+        else
+        {
+            Console.WriteLine($"Построение по росту: №3, №2, №1.");
+        }
+    }
+}
