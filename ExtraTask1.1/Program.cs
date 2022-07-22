1b61e11b266c34f63dd2238618b3dac281d929bb
BogdanKorolev <bkorolevjob@gmail.com>
2022-07-22 11:34:47 +0300

    Minor changes to Task19 added. ExtraTask1.1 code written

---
+++ ExtraTask1.1/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 1. Пользователь вводит число N (N > 0).
+Программа должна вывести N единиц на экран.
+N = 4 -> 1, 1, 1, 1
+N = 2 -> 1, 1
+*/
+
+Console.Write("Введите целое положительное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int increment = 1;
+string result = "1, ";
+
+if (N>0)
+{
+    if (N<10001)
+    {
+        for (int i = 1; i < N; i++)
+{
+    result = ($"{result}{increment}, ");
+}
+Console.Write(result.Remove(result.Length-2, 2));
+    }
+    else
+    {
+        Console.Write("Число, конечно, правильное, но ленно считать. Задайте-ка число поменьше.");
+    }
+}
+else
+{
+    Console.Write("Введено неправильное число.");
+}
--- Task21/Program.cs
+++ Task21/Program.cs
@@ -7,12 +7,12 @@ A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53
 */
 
-int Ax = 7;
-int Ay = -5;
-int Az = 0;
-int Bx = 1;
-int By = -1;
-int Bz = 9;
+int Ax = 3;
+int Ay = 6;
+int Az = 8;
+int Bx = 2;
+int By = 1;
+int Bz = -7;
 double distanceAB = Math.Pow((Math.Pow(Bx - Ax, 2))+(Math.Pow(By - Ay, 2))+(Math.Pow(Bz - Az, 2)), 0.5);
 double result = Math.Round(distanceAB, 2);
 Console.WriteLine(result);
