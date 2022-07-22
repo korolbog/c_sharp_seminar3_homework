7b9741cdb650b573a510be7e3d66f9ad0f8f078c
BogdanKorolev <bkorolevjob@gmail.com>
2022-07-22 15:57:48 +0300

    ExtraTask1.4 code written

---
+++ ExtraTask1.4/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 4.
+Дано N секунд.
+Вывести время в формате часы:минуты:секунды.
+N = 72334 -> 20:5:34 
+*/
+int N = 72334;
+int seconds = N % 60;
+int minutes = N / 60; 
+int hours = 0;
+string result = string.Empty;
+
+if (minutes < 60)
+{
+    result = $"{hours}:{minutes}:{seconds}";
+}
+else
+{
+    hours = minutes / 60;
+    minutes = minutes % 60;
+    result = $"{hours}:{minutes}:{seconds}";
+}
+Console.WriteLine(result);
